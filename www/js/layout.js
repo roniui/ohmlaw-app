2e6e798d089a529bd5e55b5c6334194abfd18226
roniui <164195422+roniui@users.noreply.github.com>
2026-02-13 08:54:29 +0530

    Implement viewport meta tag in layout.js
    Add viewport meta tag for responsive design.

--- www/js/layout.js
+++ www/js/layout.js
@@ -111,5 +111,16 @@ function toggleSidebar() {
     overlay.classList.toggle("active");
 }
 
+(function() {
+    var viewport = document.querySelector('meta[name="viewport"]');
+    
+    if (!viewport) {
+        viewport = document.createElement('meta');
+        viewport.name = "viewport";
+        document.head.appendChild(viewport);
+    }
+
+    viewport.content = "width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no";
+})();
 // Run Layout
 loadLayout();
